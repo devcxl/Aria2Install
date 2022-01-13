2993b151402ec81752940f112117ed8c0fb72df7
devcxl <orange.cxl@qq.com>
2022-01-13 18:52:05 +0800

    bugfix: download path

--- src/config_conf.sh
+++ src/config_conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 config_conf(){
   sed -i "s/%PORT%/$RPC_PORT/g" /etc/aria2/aria2.conf
-  sed -i "s/%DOWNLOAD_PATH%/$DOWNLOAD_PATH/g" /etc/aria2/aria2.conf
+  sed -i "s/%DOWNLOADPATH%/$DOWNLOAD_PATH/g" /etc/aria2/aria2.conf
   sed -i "s/%UUID%/$RPC_SEC/g" /etc/aria2/aria2.conf
   sed -i "s/%ENABLE_RPC%/$ENABLE_RPC/g" /etc/aria2/aria2.conf
   echo "=> Configuration succeeded! (/etc/aria2/aria2.conf)"
